51e6993a5040a35308de98a5f54ad5f69f2b5173
nadiaolejnik <olejnikn@msu.edu>
2023-03-25 17:13:41 -0400

    added U

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nadia and MSU!
+          Learn React with Nadia and MSUUUU!
         </a>
       </header>
     </div>
