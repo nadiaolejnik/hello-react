4de86a3678d18932f6334ace6f255dc5048be878
nadiaolejnik <113300407+nadiaolejnik@users.noreply.github.com>
2023-03-27 15:20:26 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+//import logo from './logo.svg';
+import './App.css';
+
+const classes = [
+  { id: 1, title: 'MI 449: Advanced Web Development and Database Management', theme: 'web development', isAvailable: true },
+  { id: 2, title: 'MI 450: Creating Human-Centered Technology (W)', theme: 'design', isAvailable: true },
+  { id: 3, title: 'WRA 210: Introduction to Web Authoring', theme: 'web design', isAvailable: true},
+  { id: 4, title: 'GEO 206: Physical Geography', theme: 'geography', isAvailable: true},
+  { id: 5, title: 'MI 220: Methods for Understanding Users', theme: 'user research', isAvailable: true},
+  { id: 6, title: 'MI 101: Understanding Media and Information', theme: 'media', isAvailable: false},
+  { id: 7, title: 'JRN 218: Sports in Contemporary Media', theme: 'journalism', isAvailable: false},
+];
+
+function ClassTracker() {
+  const listZines = classes.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'green' : 'red'
+      }}
+     >
+      {zine.title}
+     </li> 
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+const album = {
+  title: '1989',
+  artist: 'Taylor Swift',
+  released: '2014',
+  image: 'https://upload.wikimedia.org/wikipedia/en/f/f6/Taylor_Swift_-_1989.png',
+  width: '264',
+  height: '378'
+};
+
+function Album() {
+  return (
+    <div>
+      <h2>{album.title} ({album.released})</h2>
+      <p>{album.artist}</p>
+      <img
+        className="albumCover"
+        src={album.image}
+        alt={album.title + ' cover'}
+        style={{
+          width: album.width,
+          height: album.height
+        }}
+      />
+    </div>
+  );
+}
+function SurpriseButton() {
+  return (
+    <div>
+      <h3>Click this button for a surprise!</h3>
+      <button>Surprise!</button>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClassTracker />
+        <Album />
+        <SurpriseButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
